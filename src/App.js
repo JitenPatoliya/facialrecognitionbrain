7b609c34ba2e6c843101dcf6cb2b712b3fa3a0b5
JPatoliya <jiten1310@outlook.com>
2019-04-05 12:48:42 -0500

    fac recognition working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
 import './App.css';
+
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+
+const app = new Clarifai.App({
+  apiKey: 'f0f7558592a7428b91be217eeab74239'
+ });
 
 const particleOptions = {
   particles: {
     number: {
       value: 150,
       density: {
-        enable: true,
+        enable: true, 
         value_area: 800
       }
     }
@@ -24,15 +32,43 @@ class App extends Component {
 
     this.state ={
       input: '',
+      imageUrl:'',
+      box: {},
     }
   }
 
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    let width = Number(image.width);
+    let height = Number(image.height);
+    console.log(clarifaiFace);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height),
+    };
+  }
+  
+  displayFaceBox =(box) => {
+    console.log(box);
+    this.setState({box: box});
+  }
+
   onInputChange = (event) =>{
-    console.log(event);
+    this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () =>{
-    console.log('click');
+      this.setState({imageUrl: this.state.input})
+
+      app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
   }
 
   render() {
@@ -40,14 +76,13 @@ class App extends Component {
       <div className="App">
         <Particles className='particles'  params={particleOptions} />
         <Navigation />
-        <Logo />
+        <Logo /> 
         <Rank />
         <ImageLinkForm 
           onInputChange={this.onInputChange}
           onButtonSubmit = {this.onButtonSubmit}
           />
-        {/* 
-        <FaceRecognition /> */}
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
